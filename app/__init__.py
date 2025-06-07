bcc96706b250ea3a955cbb0b9397f37fbfab2fdb
Maks <143129096+manabr0w@users.noreply.github.com>
2025-06-07 15:27:26 +0300

    Update __init__.py and removed useless reapeat

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,1 @@
-from app.config import settings
-from app.services.s3_service import S3Service
 
-s3_service = S3Service(
-    bucket_name=settings.AWS_BUCKET_NAME,
-    region_name=settings.AWS_REGION,
-    access_key=settings.AWS_ACCESS_KEY,
-    secret_key=settings.AWS_SECRET_KEY,
-)
